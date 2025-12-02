fd74ef93591e5ae1f26582f7f92f6c807c07de24
Hamidreza Amirzadeh <h.amirzadeh1378@gmail.com>
2025-12-02 23:08:04 +0330

    Added more routes and schemas

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,16 +1,44 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import (
+    EventSchema, 
+    EventListSchema, 
+    EventCreateShema, 
+    EventUpdateShema
+)
 
 router = APIRouter()
 
+
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     return {
-        "items": [1,2,3]
+        "results": [
+            {"id":1}, {"id":2}, {"id":3}
+        ],
+        "count": 3
     }
+
 
 @router.get("/{event_id}")
 def read_events(event_id: int) -> EventSchema:
+    return {
+        "id": event_id
+    }
+
+
+@router.post("/")
+def create_events(payload:EventCreateShema) -> EventSchema:
+    # print(payload)
+    data = payload.model_dump()
+    return {
+        "id": 123,
+        **data
+    }
+
+
+@router.put("/{event_id}")
+def update_events(event_id:int, payload:EventUpdateShema) -> EventSchema:
+    print(payload)
     return {
         "id": event_id
     }
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,4 +1,21 @@
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, Field
 
 class EventSchema(BaseModel):
     id: int
+    page: Optional[str] = Field(default="")
+    description: Optional[str] = Field(default="")
+
+
+class EventCreateShema(BaseModel):
+    path: str
+    description: Optional[str] = Field(default="")
+
+
+class EventUpdateShema(BaseModel):
+    description: str
+
+
+class EventListSchema(BaseModel):
+    results: List[EventSchema]
+    count: Optional[int]
